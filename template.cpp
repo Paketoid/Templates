26838f5acac8989843769b543aa03875a801159b
LeniviyPaket <vsevolod.savchenko@inbox.ru>
2019-08-05 13:01:31 +0300

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ostream& operator << (ostream& out, const vector <auto> v){
+    for (auto x : v){
+        out << x << ' ';
+    }
+
+    return out;
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+return 0;
+}
